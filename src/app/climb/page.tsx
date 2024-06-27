bef22e17deac45ecb4bac20ef252b1800191a1da
ByunghunKim <byunghun@publy.co>
2024-06-27 11:46:34 +0900

    리다이렉트 추가

--- src/app/climb/page.tsx
+++ src/app/climb/page.tsx
@@ -3,14 +3,9 @@ import ClimbCenterSection from "./components/ClimbCenterSection";
 
 const ClimbPage = async () => {
   const climbCenters = await getClimbCenters();
-  // const visitRecords = await getVisitRecords();
 
   return (
     <div className="flex flex-1 flex-col overflow-x-hidden">
-      {/* <VisitRecordSection
-        climbCenters={climbCenters}
-        visitRecords={visitRecords}
-      /> */}
       <ClimbCenterSection climbCenters={climbCenters} />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const headersList = headers();
   const host = headersList.get("X-Forwarded-Host");
-  return (
-    <div>
-      <h1>Home</h1>
-      {/* <button onClick={handleClick}>Go to Climb</button> */}
-    </div>
-  );
-  // redirect(`${host}/climb`);
+  redirect(`${host}/climb`);
 }
