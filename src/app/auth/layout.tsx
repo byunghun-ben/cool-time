07a2bcf19efa6933ee69cf0ca3df2870bfcd5003
ByunghunKim <byunghun@publy.co>
2024-07-08 19:26:34 +0900

    fix: 컴포넌트 중복 제거

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,7 @@
 import { ReactNode } from "react";
-import Nav from "../_components/Nav";
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
-  return (
-    <>
-      <Nav />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 };
 
 export default AuthLayout;
